69d553feb2c38e1a97d7e32b2470199185f885b7
souzaa00o <henriqueskts991@gmail.com>
2023-05-23 23:23:29 -0300

    Changed environment variables

--- sqs/create-event-source-mapping.sh
+++ sqs/create-event-source-mapping.sh
@@ -3,12 +3,15 @@
 QUEUE_NAME=$1
 FUNCTION_NAME=$2
 
+REGION=<REGION> #<us-east-1, us-west-1>
+ACCOUNT_ID=<ACCOUNT ID>
+PROFILE=<PROFILE ENV>
+
 ASCII_TXT=ASCII_LambdaAttacher.txt
 
 cat "$ASCII_TXT"
 
-
-aws lambda create-event-source-mapping --function-name $FUNCTION_NAME --event-source-arn arn:aws:sqs:<REGION>:<ACCOUNT ID>:$QUEUE_NAME --profile <PROFILE>
+aws lambda create-event-source-mapping --function-name $FUNCTION_NAME --event-source-arn arn:aws:sqs:$REGION:$ACCOUNT_ID:$QUEUE_NAME --profile $PROFILE
 
 echo "$FUNCTION_NAME Lambda atached ==[>] on $QUEUE_NAME Queue_\|/_ "
 
@@ -16,4 +19,4 @@ gnome-terminal -- /<PATH>/get-queue-attributes.sh $QUEUE_NAME
 
 sleep 4
 
-aws lambda list-event-source-mappings --function-name $FUNCTION_NAME --profile noverde-prd
+aws lambda list-event-source-mappings --function-name $FUNCTION_NAME --profile $PROFILE
--- sqs/get-queue-attributes.sh
+++ sqs/get-queue-attributes.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 QUEUE_NAME=$1
+
+REGION=<REGION> #<us-east-1, us-west-1>
+ACCOUNT_ID=<ACCOUNT ID>
+PROFILE=<PROFILE ENV>
+
+
 ASCII_TXT=ASCII_QueueWatcher.txt
 
 cat "$ASCII_TXT"
@@ -11,7 +17,7 @@ do
 	
 	date +%T
 
-	aws sqs get-queue-attributes --queue-url https://sqs.<REGION>.amazonaws.com/<ACCOUNT ID>/$QUEUE_NAME --attribute-names ApproximateNumberOfMessages ApproximateNumberOfMessagesNotVisible --profile <PROFILE>
+	aws sqs get-queue-attributes --queue-url https://sqs.$REGION.amazonaws.com/$ACCOUNT_ID/$QUEUE_NAME --attribute-names ApproximateNumberOfMessages ApproximateNumberOfMessagesNotVisible --profile $PROFILE
 	
 	sleep 15
 	
