1557b5669dbf6afb422251c1b7ec4d844982fbe4
souzaa00o <henriqueskts991@gmail.com>
2023-05-29 21:04:02 -0300

    ASCII art added

--- sqs/delete-event-source-mapping.sh
+++ sqs/delete-event-source-mapping.sh
@@ -6,6 +6,10 @@ REGION=<REGION> #<us-east-1, us-west-1>
 ACCOUNT_ID=<ACCOUNT ID>
 PROFILE=<PROFILE ENV>
 
+ASCII_TXT=ASCII_LambdaKiller.txt
+
+cat "$ASCII_TXT"
+
 aws lambda list-event-source-mappings --function-name $FUNCTION_NAME --profile $PROFILE
 
 sleep 2
