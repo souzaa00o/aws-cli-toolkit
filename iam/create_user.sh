db438fa65fca9f9d37bb553500a014b6e65374f0
souzaa00o <henriqueskts991@gmail.com>
2023-06-13 02:13:10 -0300

    feat: Access key creator

--- iam/create_user.sh
+++ iam/create_user.sh
@@ -3,7 +3,6 @@
 PROFILE=<PROFILE ENV>
 URL=https://signin.aws.amazon.com/
 
-
 ASCII_TXT=ASCII_CreateUser.txt
 
 cat "$ASCII_TXT"
@@ -17,19 +16,38 @@ USER_PASS=$(pwgen -y -1 15)
 
 sleep 1
 
+
 aws iam create-login-profile --user-name $USER_NAME --password $USER_PASS --password-reset-required --profile $PROFILE
 echo Criando senha de aceso a console...
+
 
 echo Grupos:
 aws iam list-groups --query 'Groups[].GroupName' --output text --profile $PROFILE
 
+echo Digite o grupo para o usuario $USER_NAME:
 sleep 1
 
-echo Digite o grupo para o usuario $USER_NAME:
 read GROUP_NAME
-
 aws iam add-user-to-group --user-name $USER_NAME --group-name $GROUP_NAME --profile $PROFILE
 
-echo ¨URL,User,Password¨ > aws_credentials_$USER_NAME.csv
-echo ¨$URL,$USER_NAME,$USER_PASS¨ >> aws_credentials_$USER_NAME.csv
+
+echo Deseja criar uma Access key para $USER_NAME ?? [Y/n]
+
+read INPUT
+CHOICE=$(echo "$INPUT" | tr '[:upper:]' '[:lower:]')
+
+if [[ "$CHOICE" == "y" ]]; then
+	JSON=$(aws iam create-access-key --user-name $USER_NAME --profile $PROFILE)
+	sleep 2
+
+	ACCESS_KEY=$(echo "$JSON" | jq -r '.AccessKey.AccessKeyId')
+	SECRET_KEY=$(echo "$JSON" | jq -r '.AccessKey.SecretAccessKey')
+	
+	echo Access key para $USER_NAME criada.
+fi
+
+
+echo ¨URL,User,Password,AccessKey,SecretKey¨ > aws_credentials_$USER_NAME.csv
+echo ¨$URL,$USER_NAME,$USER_PASS,$ACCESS_KEY,$SECRET_KEY¨ >> aws_credentials_$USER_NAME.csv
+
 echo Output aws_credentials_$USER_NAME.csv created
